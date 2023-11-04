2f7d051ba09a537b401e1aeb8713d3f9fb613760
0xndev <141341761+0xndev@users.noreply.github.com>
2023-11-04 01:06:19 +0000

    finish

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["The", "My", "Your", "Our", "A"];
+  let adjectives = ["new", "cool", "big", "free", "smart"];
+  let nouns = ["website", "blog", "domain", "page", "app"];
+  let ends = [".com", ".org", ".biz", ".io", ".net"];
+
+  let domainNames = [];
+
+  for (let pronoun of pronouns) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let end of ends) {
+          domainNames.push(`${pronoun}${adjective}${noun}${end}`);
+        }
+      }
+    }
+  }
+
+  document.body.innerHTML = `<ul>${domainNames
+    .map(domainName => `<li>${domainName}</li>`)
+    .join(" ")}</ul>`;
 };
